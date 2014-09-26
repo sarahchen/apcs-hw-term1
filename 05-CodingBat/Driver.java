36c0b4876f917c4d080a50fadb7a63354636c68d
Sarah Chen <sarah.jia.chen@gmail.com>
2014-09-26 16:52:24 -0400

    Classwork Tested in Driver

--- 05-CodingBat/Driver.java
+++ 05-CodingBat/Driver.java
@@ -3,9 +3,14 @@ public class Driver {
     public static void main(String[] args) {
 	stringStuff s = new stringStuff();
 
-	System.out.println(s.nonStart("Hello","There"));
-	System.out.println(s.makeAbba("Hi","Bye"));
-	System.out.println(s.diff21(19));
+	System.out.println("nonStart(Hello, There): "+s.nonStart("Hello","There"));
+	System.out.println("makeABBA(Hi, Bye): "+s.makeAbba("Hi","Bye"));
+	System.out.println("diff21(19): "+s.diff21(19));
+
+	System.out.println("CLASSWORK!");
+	System.out.println("nearHundred(19): "+s.nearHundred(19));
+	System.out.println("teaParty(5, 9): "+s.teaParty(5, 9));
+	System.out.println("lastDigit(123, 234, 3): "+s.lastDigit(123,234,3));
     }
 
 }
