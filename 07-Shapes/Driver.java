55d4ed028e0b4110476a0dcf4e74ed521538a845
Sarah Chen <sarah.jia.chen@gmail.com>
2014-10-03 00:36:29 -0400

    7b hw

---
+++ 07-Shapes/Driver.java
@@ -0,0 +1,15 @@
+
+public class Driver{
+    
+    public static void main(String[] args) {
+	
+	Shapes yay = new Shapes();
+	
+	System.out.println("Triangle 3");
+	System.out.print(yay.tri3(3));
+	
+	System.out.println("Diamond");
+	System.out.print(yay.diamond(5));
+    }
+
+}
