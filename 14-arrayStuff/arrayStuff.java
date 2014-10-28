8b0c309614a389a0c5369235765d34cf3d154d03
Sarah Chen <sarah.jia.chen@gmail.com>
2014-10-28 02:37:28 -0400

    hw

---
+++ 14-arrayStuff/arrayStuff.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class arrayStuff {
+    private int[] a;
+    private Random r;
+    
+    public arrayStuff(int n) {
+        a = new int[n];
+	r = new Random();
+	for (int i=0;i<a.length;i++) {
+	    a[i] = 75+r.nextInt(76);
+	}
+    }
+    
+    public int find(int n) {
+	int ans = 0;
+        for(int i=0; i<a.length; i++) {
+	    if(a[i] == n) {
+		ans = i;
+		break;
+	    }
+	    else {
+		ans = -1;
+	    }
+	}
+	return ans;
+    }
+
+    public int maxVal() {
+        int ans = -1;
+	for (int i=0;i<a.length;i++){
+	    ans = Math.max(a[i], ans);
+	}
+	return ans;
+    }
+
+    public static void main(String[] args) {
+	arrayStuff array = new arrayStuff(100);
+	System.out.println(array.find(90));
+	System.out.println(array.maxVal());
+    }
+
+}
