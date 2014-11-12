acd8b79308c07e1e635da84fba0b080044569eb9
Sarah Chen <sarah.chen@cslab1>
2014-11-12 08:41:53 -0500

    arraylist

---
+++ 15-sarray/Driver.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    
+    public static void main(String[] args) {
+ 
+	ArrayList ai = new ArrayList<Integer>();
+	Random r = new Random();
+	for(int i=0; i<20; i++) {
+	    ai.add(r.nextInt(10));
+	}
+	System.out.println(ai);
+
+        int i = 0;
+	while(i<ai.size()-1) {
+	    if(ai.get(i) == ai.get(i+1)) {
+		ai.remove(i);
+	    } else {
+		i = i + 1;
+	    }
+	}
+	System.out.println(ai);
+    }
+}
