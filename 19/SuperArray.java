a82202013aa3f7b510b48dfb326bf293bac10565
Sarah Chen <sarah.chen@cslab1>
2014-12-03 08:42:16 -0500

    yay

--- 19/SuperArray.java
+++ 19/SuperArray.java
@@ -26,19 +26,21 @@ public class SuperArray {
 	return s;
     }
 
-    public void reAdd(int start) {
-	String[] hold = new String[index + 10];
+    public String[] reAdd(int start, int end, int expand) {
+	String[] hold = new String[(end - start) + expand];
+	int x = 0;
 	for(int i=start;i<data.length;i++) {
-	    hold[i] = data[i];
+	    hold[x] = data[i];
+	    x++;
 	}
-	data = hold;
+        return hold;
     }
     
     public boolean add(String s) {
 	try {
 	    data[index + 1] = s;
 	} catch (Exception e) {
-	    reAdd(0);
+	    data = reAdd(0,data.length,10);
 	    data[index + 1] = s;
 	}
 	index = index + 1;
@@ -46,7 +48,43 @@ public class SuperArray {
     }
 
     public void add(int i, String s) {
-	
+        String[] hold = reAdd(i,data.length,0);
+        try {
+	    data[i] = s;
+	} catch (Exception e) {
+	    data = reAdd(0,data.length,5);
+	    data[i] = s;
+	}
+        data = reAdd(0,data.length,1);
+	int x = i + 1;
+	for(int y=0;y<hold.length;y++) {
+	    data[x] = hold[y];
+	    x++;
+	} 
+    }
+
+    public String get(int i) {
+	return data[i];
+    }
+
+    public void set(int i, String s) {
+	data[i] = s;
+    }
+
+    public int size() {
+	int size = 0;
+	for(int i=0;i<data.length;i++) {
+	    if(data[i]) {
+		size = size + 1;
+	    }
+	}
+	return size;
+    }
+
+    public String remove(int i) {
+	String s = data[i];
+	data[i] = null;
+	return s;
     }
 
     /* -------- MAIN --------- */
@@ -55,11 +93,14 @@ public class SuperArray {
 	System.out.println(s);
 	s.add("Hello");
 	s.add("Goodbye");
-	s.add("yahoo");
-	s.add("asdjfkls");
 	s.add("yay");
-	s.add("meh");
 	System.out.println(s);
+	s.add(2, "meh");
+	System.out.println(s);
+	s.add(5, "yay");
+	s.add(5, "no");
+	System.out.println(s);
+	System.out.println(s.size());
     }
 
 }
